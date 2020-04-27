894ded8377ceefbaa4c1542b7794f5f7f32fb374
ArturOle <artur.oleksinski99@gmail.com>
2020-04-28 00:23:41 +0200

    Update

--- TypingTrainerOOP.py
+++ TypingTrainerOOP.py
@@ -11,12 +11,19 @@ class MainPanel(wx.Panel):
         self.Centre()
 
     def _init_ui(self):
+        self.SetBackgroundColour(wx.WHITE)
         sizer_vertical = wx.BoxSizer(wx.VERTICAL)
-        sizer_vertical.Add((0, 300), 0, wx.TOP)
+        logo = wx.Image("logo.bmp", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        logo_bit = wx.StaticBitmap(self, -1, logo, size=(logo.GetWidth(), logo.GetHeight()))
+        sizer_vertical.Add(logo_bit, 0, wx.ALIGN_CENTER, 0)
 
         play_button = wx.Button(self, 0, "Play")
-        sizer_vertical.Add(play_button, 0, wx.ALIGN_CENTRE, 0)
+        sizer_vertical.Add(play_button, 0, wx.ALIGN_CENTER, 0)
         play_button.Bind(wx.EVT_BUTTON, self.change_to_play)
+
+        play_button = wx.Button(self, 0, "Options")
+        sizer_vertical.Add(play_button, 0, wx.ALIGN_CENTER, 0)
+        play_button.Bind(wx.EVT_BUTTON, self.change_to_options)
 
         exit_button = wx.Button(self, 0, "Exit")
         sizer_vertical.Add(exit_button, 0, wx.ALIGN_CENTRE)
@@ -27,6 +34,9 @@ class MainPanel(wx.Panel):
     def change_to_play(self, event):
         self.parent.switch_panel(self.parent.main_panel, self.parent.play_panel)
         self.parent.play_panel.timer_on()
+
+    def change_to_options(self, event):
+        self.parent.switch_panel(self.parent.main_panel, self.parent.option_panel)
 
     def on_exit(self, event):
         self.parent.Close(True)
