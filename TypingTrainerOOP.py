f47515cbb749e26398e596afe7a5b37f779c0fc2
ArturOle <artur.oleksinski99@gmail.com>
2020-05-02 13:46:20 +0200

    Update 1: Options done

--- TypingTrainerOOP.py
+++ TypingTrainerOOP.py
@@ -47,8 +47,8 @@ class MainPanel(wx.Panel):
 class PlayPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent=parent)
-        self.parent = parent
         self.SetSize((640, 420))
+        self.parent = parent
         self.SetOwnBackgroundColour(wx.Colour(60, 60, 60, 0))
         self.sizer_vertical = wx.BoxSizer(wx.VERTICAL)
 
@@ -64,7 +64,7 @@ class PlayPanel(wx.Panel):
         self.info_text = wx.StaticText(self,
                                        label="Prepare for typing!",
                                        style=wx.ALIGN_CENTER,
-                                       size=(wx.Size.GetWidth(self.Size), 100))
+                                       size=(wx.Size.GetWidth(self.parent.Size), 100))
         self.info_text.SetFont(font)
         self.sizer_vertical.Add(self.info_text, 0, wx.CENTER, 0)
 
@@ -145,7 +145,7 @@ class PlayPanel(wx.Panel):
     def get_random_line(self):
         index = randint(0, self.get_volume())
         try:
-            with open("storage.txt", "r") as file:
+            with open("storageM.csv", "r") as file:
                 for i, line in enumerate(file):
                     if index == i:
                         line = line.split(';')
@@ -157,7 +157,7 @@ class PlayPanel(wx.Panel):
 
     def get_volume(self):
         try:
-            with open("storage.txt", "r") as file:
+            with open("storageM.csv", "r") as file:
                 volume = 0
                 for line in file:
                     volume += 1
@@ -172,16 +172,81 @@ class PlayPanel(wx.Panel):
 class OptionsPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent=parent)
+        self.SetSize((640, 480))
+        self.width = self.Size[0]
         self.parent = parent
-        self._init_ui()
+        self.SetSize((parent.width, parent.height))
 
-    def _init_ui(self):
-        self.get_specs()
+        self.SetOwnBackgroundColour(wx.Colour(255, 255, 255, 0))
 
-    def get_specs(self):
+        sizer_vertical = wx.BoxSizer(wx.VERTICAL)
+        sizer_horizontal = wx.BoxSizer(wx.HORIZONTAL)
+
+        specs = self._get_specs()
+
+        sizer_vertical.AddSpacer(20)
+        sizer_horizontal.Add(wx.StaticText(self, label="Quantity of rounds"))
+        sizer_vertical.Add(sizer_horizontal, 0, wx.CENTER, 0)
+
+        self.rounds_slider = wx.Slider(self,
+                                       value=specs["Value"][1],
+                                       minValue=1,
+                                       maxValue=10,
+                                       style=wx.SL_HORIZONTAL | wx.SL_LABELS,
+                                       size=(self.width-15, 20))
+        sizer_vertical.Add(self.rounds_slider, flag=wx.CENTER | wx.TOP | wx.EXPAND)
+        self.rounds_slider.Bind(wx.EVT_SCROLL, self.Update())
+
+        self.box_text = wx.StaticText(self, label="Choose difficulty")
+        sizer_vertical.AddSpacer(40)
+        sizer_vertical.Add(self.box_text, 0, wx.CENTER, 0)
+
+        self.level_box = wx.ComboBox(self, choices=("Medium", "High"), style=wx.CB_READONLY)
+        self.level_box.SetValue(specs["Value"][0])
+        sizer_vertical.AddSpacer(10)
+        sizer_vertical.Add(self.level_box, 0, wx.CENTER, 0)
+
+        self.path_text = wx.StaticText(self, label="Storage directory")
+        sizer_vertical.AddSpacer(30)
+        sizer_vertical.Add(self.path_text, 0, wx.CENTER, 0)
+
+        self.path_control = wx.TextCtrl(self, size=(wx.Size.GetWidth(self.Size), 20))
+        self.path_control.SetValue(specs["Value"][2])
+        sizer_vertical.AddSpacer(10)
+        sizer_vertical.Add(self.path_control, 0, wx.CENTER, 0)
+
+        save_button = wx.Button(self, label="Save Changes")
+        save_button.Bind(wx.EVT_BUTTON, self._overwrite_specs)
+        sizer_vertical.AddSpacer(100)
+        sizer_vertical.Add(save_button, 0, wx.CENTER | wx.BOTTOM, 0)
+
+        specs = self._get_specs()
+        print(specs["Option"][1])
+        print(specs["Value"][1])
+
+        sizer_vertical.AddSpacer(2000)
+
+        self.SetSizerAndFit(sizer_vertical)
+        self.Layout()
+
+    @staticmethod
+    def _get_specs():
         df = pd.read_json("options.json")
-        value = (df.iat[1, 1])
-        print(value)
+        dictionary = df.to_dict()
+        return dictionary
+
+    def _overwrite_specs(self, event):
+        value_level = self.level_box.GetValue()
+        value_rounds = self.rounds_slider.GetValue()
+        value_patch = "storageM.csv"
+        print("Option 1 changed to ", value_rounds)
+        df = pd.read_json("options.json")
+        print(df)
+        df.iloc[0, 1] = value_level
+        df.iloc[1, 1] = value_rounds
+        df.iloc[2, 1] = value_patch
+        df.to_json("options.json")
+        self.parent.switch_panel(self.parent.option_panel, self.parent.main_panel)
 
 
 class Frame(wx.Frame):
@@ -192,8 +257,8 @@ class Frame(wx.Frame):
     should be shown or hide.
     """
     def __init__(self, parent, title):
-        self.width, self.height = wx.GetDisplaySize()
         super().__init__(parent=parent, title=title)
+        self.width, self.height = wx.Window.GetClientSize(self)
         self.main_panel = MainPanel(self)
 
         self.play_panel = PlayPanel(self)
@@ -202,11 +267,10 @@ class Frame(wx.Frame):
         self.option_panel = OptionsPanel(self)
         self.option_panel.Hide()
 
-        self.panel = wx.Panel()
-
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.main_panel, 1, wx.EXPAND, 1)
         self.sizer.Add(self.play_panel, 1, wx.EXPAND, 1)
+        self.sizer.Add(self.option_panel, 1, wx.EXPAND, 1)
 
         self.SetSizer(self.sizer)
 
